83bc0c8d6820776d277981560fab308502b7959a
MosutLarisa <larisa.mosut@emanuel.ro>
2023-11-10 09:28:38 +0200

    oopc-class-members

---
+++ OopClassMembers/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace OopClassMembers;
+
+internal class Persons
+{
+
+    public string FirstName = "Jhon";
+
+    public string LastName = "Doe";
+
+    // readonly field can be modified from the initialization expression (#1)
+    public readonly string CNP = "1234";
+
+    public static int PopulationCount = 0;
+
+    public const int MaxLifespanYears = 60 * 2 + 10;
+
+    //this is  a constructor
+
+    public Persons(string cnp)
+    {
+        // readonly field can be modified from the constructor (#2)
+        CNP = cnp;
+    }
+
+    public void PrintPerson()
+    {
+
+    }
+
+    public static void PrintPopulationCount()
+    {
+        
+    }
+
+
+
+
+
+
+
+}
+
---
+++ OopClassMembers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OopClassMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxiLifespan = Persons.MaxLifespanYears;
+            Persons p = new Persons();
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.CNP);
+
+            
+            Persons.PopulationCount = 1;
+
+        }
+    }
+}
